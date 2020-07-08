b9545782303bbc8c5948d8173e9a9d70ad7bb140
Quentin <pamartqu@gmail.com>
2020-07-08 16:38:37 +0200

    pacman controls

--- PacManECS/Assets/Scripts/PacManController.cs
+++ PacManECS/Assets/Scripts/PacManController.cs
@@ -1,18 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PacManController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class PacManController : MonoBehaviour {
+    public int speed;
+    private Vector3 _direction = Vector3.zero;
+    void Update() {
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            _direction = Vector3.forward;
+        } else if (Input.GetKey(KeyCode.DownArrow)) {
+            _direction = Vector3.back;
+        } else if (Input.GetKey(KeyCode.LeftArrow)) {
+            _direction = Vector3.left;
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            _direction = Vector3.right;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (_direction != Vector3.zero && !Physics.BoxCast(transform.position, Vector3.one/2, _direction, Quaternion.identity, 0.1f, 1 << 8)) {
+            Vector3 position = transform.position;
+            transform.LookAt(position + _direction);
+            transform.position = Vector3.MoveTowards(position, position+_direction, Time.deltaTime*speed);
+        }
     }
 }
