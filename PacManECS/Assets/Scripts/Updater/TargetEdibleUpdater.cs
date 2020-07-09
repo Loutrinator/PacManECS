5b5118f9d68722ebc7413cdd323568395bf550f5
Quentin <pamartqu@gmail.com>
2020-07-09 13:19:38 +0200

    merge fixes

--- PacManECS/Assets/Scripts/Updater/TargetEdibleUpdater.cs
+++ PacManECS/Assets/Scripts/Updater/TargetEdibleUpdater.cs
@@ -6,7 +6,6 @@ namespace Updater {
     public class TargetEdibleUpdater : IUpdater {
         public override void DoUpdate() {
             for (int i = TAccessor<FollowTarget>.Instance.Modules.Count - 1; i >= 0; --i) {
-                print("in update");
                 TargetEdibleModule module = TAccessor<TargetEdibleModule>.Instance.Modules[i];
             }
         }
