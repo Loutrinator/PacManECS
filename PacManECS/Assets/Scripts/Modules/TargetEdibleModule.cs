6fa78857dd438ccf57062d909c4a6dcff9afa970
Quentin <pamartqu@gmail.com>
2020-07-09 12:44:54 +0200

    Edible files

---
+++ PacManECS/Assets/Scripts/Modules/TargetEdibleModule.cs
@@ -0,0 +1,7 @@
+﻿namespace Modules {
+    public class TargetEdibleModule : Module {
+        public override void Register() {
+            TAccessor<TargetEdibleModule>.Instance.Add(this);
+        }
+    }
+}
---
+++ PacManECS/Assets/Scripts/Updater/TargetEdibleUpdater.cs
@@ -0,0 +1,12 @@
+﻿using Modules;
+using UnityEngine;
+
+namespace Updater {
+    public class TargetEdibleUpdater : MonoBehaviour {
+        void Update() {
+            for (int i = TAccessor<FollowTarget>.Instance.Modules.Count - 1; i >= 0; i--) {
+                TargetEdibleModule module = TAccessor<TargetEdibleModule>.Instance.Modules[i];
+            }
+        }
+    }
+}
