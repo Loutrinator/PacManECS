62d039781efc8c2f01b55687c144851b0c27d1b4
Quentin <pamartqu@gmail.com>
2020-07-09 14:38:19 +0200

    fix merge

--- PacManECS/Assets/Scripts/Updater/TargetEdibleUpdater.cs
+++ PacManECS/Assets/Scripts/Updater/TargetEdibleUpdater.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 namespace Updater {
     public class TargetEdibleUpdater : IUpdater {
         public override void DoUpdate() {
-            for (int i = TAccessor<FollowTarget>.Instance.Modules.Count - 1; i >= 0; --i) {
+            for (int i = TAccessor<TargetEdibleModule>.Instance.Modules.Count - 1; i >= 0; --i) {
+                Debug.Log(i);
                 TargetEdibleModule module = TAccessor<TargetEdibleModule>.Instance.Modules[i];
             }
         }
