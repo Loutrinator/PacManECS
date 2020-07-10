9a22002a2060bdf3859bed7d74d3c07661ee1a57
Quentin <pamartqu@gmail.com>
2020-07-10 12:00:31 +0200

    merge fixes

--- PacManECS/Assets/Scripts/Updater/PlayerFollowTargetUpdater.cs
+++ PacManECS/Assets/Scripts/Updater/PlayerFollowTargetUpdater.cs
@@ -4,18 +4,14 @@ using UnityEngine;
 
 namespace Updater {
     public class PlayerFollowTargetUpdater : IUpdater {
-        public float detectionDistance = 10f;
         public override void DoUpdate() {
             if (!GameManager.Instance.GameIsRunning) return;
             for (int i = 0; i < TAccessor<PlayerFollowTarget>.Instance.Modules.Count; ++i) {
                 PlayerFollowTarget follower = TAccessor<PlayerFollowTarget>.Instance.Modules[i];
 
                 GetClosestEnemy(follower);
-                if (follower.target != null /*&& GameManager.Instance.isFruitActive*/) {
-                    RefreshTargetDestination(follower);
-                    TAccessor<TargetEdibleModule>.Instance.Get(follower).doChase = false;
-                } else {
-                    TAccessor<TargetEdibleModule>.Instance.Get(follower).doChase = true;
+                if (follower.target != null && GameManager.Instance.isFruitActive) {
+                    TAccessor<TargetEdibleModule>.Instance.Get(follower).target = follower.target;
                 }
             }
         }
@@ -26,9 +22,9 @@ namespace Updater {
             for (int i = TAccessor<FollowTarget>.Instance.Modules.Count - 1; i >= 0; --i) {
                 FollowTarget enemy = TAccessor<FollowTarget>.Instance.Modules[i];
                 float curDist = Vector3.Distance(enemy.transform.position, follower.transform.position);
-                if (curDist < distance && curDist < detectionDistance) {
+                if (curDist < distance) {
                     distance = curDist;
-                    follower.target = TAccessor<ITarget>.Instance.Get(enemy);
+                    follower.target = enemy;
                 }
             }
         }
